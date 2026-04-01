f8760e1da16fefdd7a918834eba3b6e90304e79b
shola-devv <creekemmanuelf@gmail.com>
2026-04-01 23:13:19 +0100

    vd 8

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -1,2 +1,14 @@
 declare module 'framer-motion';
 declare module 'nodemailer';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      profile?: number | null;
+    };
+  }
+}
