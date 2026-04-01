ed24a8ccc0893004118eeb232d32b961c8746ede
shola-devv <creekemmanuelf@gmail.com>
2026-04-01 23:20:58 +0100

    vd 9

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,4 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/nextAuthOptions";
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);
